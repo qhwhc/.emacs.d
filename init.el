c0ea247e9472d50bc8cd5bd4048272fa5e1a1750
ccc <304136830@qq.com>
2019-03-27 07:23:56 +0800

    add en

--- init.el
+++ init.el
@@ -8,6 +8,7 @@
 ;; add other el file
 (add-to-list 'load-path "~/.emacs.d/lisp/")
 (add-to-list 'load-path "~/.emacs.d/lisp/otherEl")
+(add-to-list 'load-path "~/.emacs.d/lisp/LEN")
 ;;Function
 (org-babel-load-file (expand-file-name "lisp/init-function.org" user-emacs-directory))
 
@@ -27,9 +28,25 @@
 (org-babel-load-file (expand-file-name "lisp/init-keybindings.org" user-emacs-directory))
 (require 'init-keybindings)
 
+;;init-abbrev-table
+(org-babel-load-file (expand-file-name "lisp/init-abbrev-table.org" user-emacs-directory))
+(require 'init-abbrev-table)
+
 ;;init-ui
 (org-babel-load-file (expand-file-name "lisp/init-ui.org" user-emacs-directory))
 (require 'init-ui)
+
+;;python en
+;;(org-babel-load-file (expand-file-name "lisp/init-python.org" user-emacs-directory))
+;;(require 'init-python)
+
+;;Js en
+(org-babel-load-file (expand-file-name "lisp/LEN/init-js.org" user-emacs-directory))
+(require 'init-js)
+
+;;Html en
+(org-babel-load-file (expand-file-name "lisp/LEN/init-html.org" user-emacs-directory))
+(require 'init-html)
 
 ;;custom-file
 (setq custom-file (expand-file-name "lisp/custom.el" user-emacs-directory))
